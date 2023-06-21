6c9c165b103de973d604cb65b3d6bb29d7a1b89b
Carlos Julián Pacheco Prada <cjpachecop@ufpso.edu.co>
2023-06-20 19:17:18 -0500

    estudiando en casa vue-tailwind

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+import TaskComponent from '@/components/TaskComponent.vue';
+import {ref} from 'vue';
+
+let newTask
+
+const TaskList = ref([
+  'levantarse',
+  'Cruzar el semaforo',
+  'Salvese quien pueda'
+])
+
+const addTask = () => {
+  TaskList.value.unshift(newTask)
+
+  const precio = [20,32,5]
+  let total = 0
+  for (let i=0; i< precio.length; i++)
+  {
+    total = total + precio [i]
+  }
+console.log('Todo suma', total)
+
+}
+
+const deletefromarray= (index) => {
+  
+  TaskList.value.splice(index,1)
+  console.log('kill',TaskList)
+}
+
+</script>
+
+<template>
+<div class="px-4 py-3 mx-auto mt-12 space-y-2 overflow-hidden rounded shadow-md bg-gray-50 w-96">
+<h1 class="text-2xl font-semibold">Lista de tareas</h1>
+
+<div class="flex">
+  <input type="text" class="w-4/5 h-8 px-2 py-1 border border-r-0 border-green-600 rounded rounded-r-none" v-model="newTask">
+  <span class="w-1/5 h-8 text-xl font-bold text-center text-white bg-green-600 rounded rounded-l-none leading-0 hover:bg-green-700 hover:cursor-pointer" 
+  @click="addTask">
+    +
+  </span>
+</div>
+
+<TaskComponent v-for="(task, index) in TaskList " 
+:key="index" 
+:name="task" 
+:index="index" 
+@emitdeleteTask="deletefromarray"/>
+
+</div>
+
+
+
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+    name: {
+
+type: String,
+default: 'No tengo nombre'
+},
+index: {
+    type:Number
+}
+})
+
+const emit = defineEmits([
+    'emitdeleteTask'
+])
+const deleteTask = (index) => {
+    console.log('Borrar',index)
+    emit('emitdeleteTask',index)
+}
+
+</script>
+
+<template>
+    <div class="flex justify-between px-4 py-2 bg-gray-200 rounded">
+        {{ props.name }}
+        <span class="inline-block px-2 text-white bg-red-400 rounded hover:bg-red-600 hover:cursor-pointer" @click="deleteTask(props.index)">
+            x
+        </span>
+    </div>
+</template>
